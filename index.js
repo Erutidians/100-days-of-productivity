8189b5f305b587e0ae07990a7a471fb0b05948a2
Zai Santillan (Palskie) <zaisantillan14@gmail.com>
2022-07-07 04:54:43 +0800

    chore(deps): bump @erutidians/auto-dop from 1.0.0 to 1.0.2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { run } from '@erutidians/auto-dop';
 
+// ! change this.
 const user = {
-  name: 'Palskie',
-  github: 'plskz',
-  start: '7/6/2022', // M/D/YYYY
-  round: 1,
+  name: 'Palskie',   // change this to any name that you're comfortable with.
+  github: 'plskz',   // your github username.
+  start: '6/1/2022', // the date that you will start this challenge (e.g. m/d/yyyy)
 };
 
 run(user);
